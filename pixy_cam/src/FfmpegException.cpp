0ccab963b7d6f08b78b181452b8fae992e7c277c
seth Hendrick <seth@hahli.lan.thenaterhood.com>
2024-08-26 22:14:02 -0400

    Remove references from libav

--- pixy_cam/src/FfmpegException.cpp
+++ pixy_cam/src/FfmpegException.cpp
@@ -1,19 +1,9 @@
 #include <string>
 
-extern "C" {
-#include <libavutil/avutil.h>
-}
-
 #include "FfmpegException.h"
 
 namespace pixy_cam
 {
-    FfmpegException::FfmpegException( int errorCode ) :
-        FfmpegException::FfmpegException( GetErrorCodeMessage( errorCode ) )
-    {
-        
-    }
-
     FfmpegException::FfmpegException( const std::string& message ) :
         std::runtime_error( message )
     {
@@ -21,18 +11,5 @@ namespace pixy_cam
 
     FfmpegException::~FfmpegException()
     {
-    }
-
-    std::string FfmpegException::GetErrorCodeMessage( int errorCode )
-    {
-        const size_t bufferSize = 255;
-        char errorBuffer[bufferSize];
-        int return_code = av_strerror( errorCode, errorBuffer, bufferSize );
-        if( return_code < 0 )
-        {
-            return std::string( "Unknown Error: " ) + std::to_string( errorCode );
-        }
-
-        return std::string( errorBuffer );
     }
 }
