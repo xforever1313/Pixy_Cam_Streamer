9ac78cd977db34565ae5ed9162964738fc522806
Seth Hendrick <seth@shendrick.net>
2024-09-01 17:42:48 -0400

    Added AGPL license

--- include/PixyCamera.h
+++ include/PixyCamera.h
@@ -1,3 +1,21 @@
+//
+// Pixy Cam Streamer - Stream a Pixy Camera to a RTMP server
+// Copyright (C) 2024 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 #ifndef PIXY_CAMERA_H_
 #define PIXY_CAMERA_H_
 
